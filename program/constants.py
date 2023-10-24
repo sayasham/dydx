f58164c17eaf04e19b6adeea4bcf92ea18caa7f3
Sham <sayasham@gmail.com>
2023-10-25 05:54:32 +0800

    finish

--- program/constants.py
+++ program/constants.py
@@ -5,16 +5,16 @@ from decouple import config
 MODE = "DEVELOPMENT"
 
 # Close all open positions and orders
-ABORT_ALL_POSITIONS = False
+ABORT_ALL_POSITIONS = True
 
 # Find Cointegrated Pairs
-FIND_COINTEGRATED = True
+FIND_COINTEGRATED = False
 
 # Manage Exits
-MANAGE_EXITS = True
+MANAGE_EXITS = False
 
 # Place Trades
-PLACE_TRADES = True
+PLACE_TRADES = False
 
 # Resolution
 RESOLUTION = "1HOUR"
--- program/func_private.py
+++ program/func_private.py
@@ -1,18 +1,44 @@
 from datetime import datetime, timedelta
-import time
-from pprint import pprint
 from func_utils import format_number
+import time
+import json
+
+from pprint import pprint
+
+# Get existing open positions
+def is_open_positions(client, market):
+
+  # Protect API
+  time.sleep(0.2)
+
+  # Get positions
+  all_positions = client.private.get_positions(
+    market=market,
+    status="OPEN"
+  )
+
+  # Determine if open
+  if len(all_positions.data["positions"]) > 0:
+    return True
+  else:
+    return False
+
+# Check order status
+def check_order_status(client, order_id):
+  order = client.private.get_order_by_id(order_id)
+  return order.data["order"]["status"]
+  
+
 
 # Place market order
 def place_market_order(client, market, side, size, price, reduce_only):
-    # Get Position Id
+  # Get Position Id
   account_response = client.private.get_account()
   position_id = account_response.data["account"]["positionId"]
 
   # Get expiration time
   server_time = client.public.get_time()
-  expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", '+00:00')) + timedelta(seconds=90)
-  
+  expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z","+00:00")) + timedelta(seconds=70)
 
   # Place an order
   placed_order = client.private.create_order(
@@ -29,13 +55,16 @@ def place_market_order(client, market, side, size, price, reduce_only):
     reduce_only=reduce_only
   )
 
-# Return result
+  # print(placed_order.data)
+
+  # Return result
   return placed_order.data
 
 
 # Abort all open positions
 def abort_all_positions(client):
-     # Cancel all orders
+  
+  # Cancel all orders
   client.private.cancel_all_orders()
 
   # Protect API
@@ -64,16 +93,15 @@ def abort_all_positions(client):
       # Determine Side
       side = "BUY"
       if position["side"] == "LONG":
-         side = "SELL"
-      print(market,side)
-    
-       # Get Price
+        side = "SELL"
+
+      # Get Price
       price = float(position["entryPrice"])
       accept_price = price * 1.7 if side == "BUY" else price * 0.3
       tick_size = markets["markets"][market]["tickSize"]
       accept_price = format_number(accept_price, tick_size)
 
- # Place order to close
+      # Place order to close
       order = place_market_order(
         client,
         market,
@@ -88,6 +116,11 @@ def abort_all_positions(client):
 
       # Protect API
       time.sleep(0.2)
+
+    # Override json file with empty list
+    bot_agents = []
+    with open("bot_agents.json", "w") as f:
+      json.dump(bot_agents, f)
 
     # Return closed orders
     return close_orders
